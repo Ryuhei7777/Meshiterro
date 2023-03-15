e06ea3b3368a8d58273f0cac331ad6059d065178
Ryuhei7777 <124151763+Ryuhei7777@users.noreply.github.com>
2023-03-15 06:40:22 +0000

    [Finish]4-26アプリケーション実装完了

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -4,6 +4,12 @@ class PostImage < ApplicationRecord
   has_many :post_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
 
+  #shop_nameが存在しているかを確認するバリデーション
+  validates :shop_name, presence: true
+  
+  #imageが存在しているかを確認するバリデーション
+  validates :image, presence: true  
+  
   def get_image
     unless image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
@@ -15,5 +21,5 @@ class PostImage < ApplicationRecord
   def favorited_by?(user)
     favorites.exists?(user_id: user.id)
   end
-  
+ 
 end
